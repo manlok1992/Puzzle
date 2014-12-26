c09701d14d149080039cef64631ee80a99a39693
manlok1992 <manlok123@hotmail.com>
2014-12-26 12:02:38 +0800

    create puzzle

---
+++ Puzzle/Assets/Scripts/Clone.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Clone : MonoBehaviour {
+	GameObject[,] puzzles;
+	static readonly int ballNum = 4;
+	public GameObject[] prefab;
+	int[,] randomIndex;
+	// Use this for initialization
+	void Start () {
+		puzzles = new GameObject[ballNum,ballNum];
+		for(int i = 0; i < ballNum; i++) {
+			for(int j = 0; j < ballNum; j++) {	
+				var ball = GameObject.Instantiate(prefab[randomBall(i,j)], new Vector3(-3+i, -3+j, 0), Quaternion.identity);
+				puzzles[i,j] = (GameObject)ball;
+			}
+		}
+	}
+
+	int randomBall(int i, int j) {
+		randomIndex = new int[ballNum,ballNum];
+		randomIndex[i,j] = UnityEngine.Random.Range(0, prefab.Length);
+		return randomIndex[i,j];
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Puzzle/Assets/Scripts/TouchEvent.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchEvent : MonoBehaviour {
+	public bool isTouchDown = false;
+	enum Direction{LEFT, RIGHT, UP, DOWN, NONE};
+	Direction dir;
+	// Use this for initialization
+	void Start () {
+		dir = Direction.NONE;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(isTouchDown) {
+			if(Input.GetAxis("Mouse X") < -0.3) {
+				//Left
+				dir = Direction.LEFT;
+			}
+			if(Input.GetAxis("Mouse X") > 0.3) {
+				//Right
+				dir = Direction.RIGHT;
+			}
+			if(Input.GetAxis("Mouse Y") < -0.3) {
+				//Down
+				dir = Direction.DOWN;
+			}
+			if(Input.GetAxis("Mouse Y") > 0.3) {
+				//Up
+				dir = Direction.UP;
+			}
+			if(Input.GetAxis("Mouse X") < 0.3 && Input.GetAxis("Mouse X") > -0.3 && 
+			   Input.GetAxis("Mouse Y") < 0.3 && Input.GetAxis("Mouse Y") > -0.3) {
+				dir = Direction.NONE;
+			}
+		}
+	}
+
+	void OnMouseDown() { 
+		isTouchDown = true;
+	}
+
+	void OnMouseUp() {
+		isTouchDown = false;
+	}
+}
